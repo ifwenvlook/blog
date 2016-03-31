24898ed60cc7a2988cb733c4587563a52531c350
centos_wen <ifwenvlook@gmail.com>
2016-03-31 10:14:46 +0800

    centos_blog V1.0

---
+++ app/auth/forms.py
@@ -0,0 +1,72 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
+
+
+class LoginForm(Form):
+    email = StringField('邮箱', validators=[Required(), Length(1, 64),
+                                             Email()])
+    password = PasswordField('密码', validators=[Required()])
+    remember_me = BooleanField('记住我')
+    submit = SubmitField('登录')
+
+
+class RegistrationForm(Form):
+    email = StringField('邮箱', validators=[Required(), Length(1, 64),
+                                           Email()])
+    username = StringField('用户名', validators=[
+        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                          'Usernames must have only letters, '
+                                          'numbers, dots or underscores')])
+    password = PasswordField('密码', validators=[
+        Required(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('确认密码', validators=[Required()])
+    submit = SubmitField('注册')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email已经被注册过.请更换')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已存在')
+
+
+class ChangePasswordForm(Form):
+    old_password = PasswordField('旧密码', validators=[Required()])
+    password = PasswordField('新密码', validators=[
+        Required(), EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('确认新的密码', validators=[Required()])
+    submit = SubmitField('更新密码')
+
+
+class PasswordResetRequestForm(Form):
+    email = StringField('邮箱', validators=[Required(), Length(1, 64),
+                                             Email()])
+    submit = SubmitField('重置密码')
+
+
+class PasswordResetForm(Form):
+    email = StringField('邮箱', validators=[Required(), Length(1, 64),
+                                             Email()])
+    password = PasswordField('新密码', validators=[
+        Required(), EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('确认新的密码', validators=[Required()])
+    submit = SubmitField('重置密码')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first() is None:
+            raise ValidationError('不合法的Email地址')
+
+
+class ChangeEmailForm(Form):
+    email = StringField('新邮箱', validators=[Required(), Length(1, 64),
+                                                 Email()])
+    password = PasswordField('密码', validators=[Required()])
+    submit = SubmitField('更新邮箱')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已被使用')
---
+++ app/main/forms.py
@@ -0,0 +1,61 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, TextAreaField, BooleanField, SelectField,\
+    SubmitField
+from wtforms.validators import Required, Length, Email, Regexp
+from wtforms import ValidationError
+from flask.ext.pagedown.fields import PageDownField
+from ..models import Role, User
+
+
+class NameForm(Form):
+    name = StringField('你的名字', validators=[Required()])
+    submit = SubmitField('提交')
+
+
+class EditProfileForm(Form):
+    name = StringField('姓名', validators=[Length(0, 64)])
+    location = StringField('地址', validators=[Length(0, 64)])
+    about_me = TextAreaField('关于我')
+    submit = SubmitField('提交')
+
+
+class EditProfileAdminForm(Form):
+    email = StringField('电子邮箱(Email)', validators=[Required(), Length(1, 64),
+                                             Email()])
+    username = StringField('用户名', validators=[
+        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                          'Usernames must have only letters, '
+                                          'numbers, dots or underscores')])
+    confirmed = BooleanField('Confirmed')
+    role = SelectField('Role', coerce=int)
+    name = StringField('姓名', validators=[Length(0, 64)])
+    location = StringField('地址', validators=[Length(0, 64)])
+    about_me = TextAreaField('关于我')
+    submit = SubmitField('提交')
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name)
+                             for role in Role.query.order_by(Role.name).all()]
+        self.user = user
+
+    def validate_email(self, field):
+        if field.data != self.user.email and \
+                User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email已经被使用.')
+
+    def validate_username(self, field):
+        if field.data != self.user.username and \
+                User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已存在')
+
+
+class PostForm(Form):    
+    head = StringField('标题', validators=[Required(), Length(1, 30) ])
+    body = PageDownField("博客内容", validators=[Required()])    
+    submit = SubmitField('提交')
+
+
+class CommentForm(Form):
+    body = StringField('输入你的评论', validators=[Required()])
+    submit = SubmitField('提交')
