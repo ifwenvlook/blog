20a2fbba73d493901b1a78071f15fb924fce91f3
home_wen <ifwenvlook@163.com>
2016-04-22 21:38:57 +0800

    weibo

---
+++ weibo.py
@@ -0,0 +1,77 @@
+from flask import Flask, redirect, url_for, session, request, jsonify
+from flask_oauthlib.client import OAuth
+
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'development'
+oauth = OAuth(app)
+
+weibo = oauth.remote_app(
+    'weibo',
+    consumer_key='1523741413',
+    consumer_secret='dadfdcb4d08fa3825b951086c67d2e65',
+    request_token_params={'scope': 'email,statuses_to_me_read'},
+    base_url='https://api.weibo.com/2/',
+    authorize_url='https://api.weibo.com/oauth2/authorize',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://api.weibo.com/oauth2/access_token',
+    # since weibo's response is a shit, we need to force parse the content
+    content_type='application/json',
+)
+
+
+@app.route('/')
+def index():
+    if 'oauth_token' in session:
+        access_token = session['oauth_token'][0]
+        resp = weibo.get('statuses/home_timeline.json')
+        return jsonify(resp.data)
+    return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+    return weibo.authorize(callback=url_for('authorized',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('oauth_token', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/login/authorized')
+def authorized():
+    resp = weibo.authorized_response()
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['oauth_token'] = (resp['access_token'], '')
+    return redirect(url_for('index'))
+
+
+@weibo.tokengetter
+def get_weibo_oauth_token():
+    return session.get('oauth_token')
+
+
+def change_weibo_header(uri, headers, body):
+    """Since weibo is a rubbish server, it does not follow the standard,
+    we need to change the authorization header for it."""
+    auth = headers.get('Authorization')
+    if auth:
+        auth = auth.replace('Bearer', 'OAuth2')
+        headers['Authorization'] = auth
+    return uri, headers, body
+
+weibo.pre_request = change_weibo_header
+
+
+if __name__ == '__main__':
+    app.run()
