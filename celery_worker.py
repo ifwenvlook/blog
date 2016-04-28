c50e154a233dab0b04f36bd21c26decf171ca35f
ifwenvlook <ifwenvlook@163.com>
2016-04-28 14:42:49 +0800

    celery mail upgrade

--- celery_worker.py
+++ celery_worker.py
@@ -3,3 +3,4 @@ from app import celery, create_app
 
 app = create_app('default')
 app.app_context().push()
+#start celery worker -A celery_worker.celery -l INFO  /celery worker -A celery_worker.celery --loglevel=info
