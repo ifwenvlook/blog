5a8ac537b6b34b69f76872c7e7e9109304d37230
wen_centos <ifwenvlook@gmail.com>
2016-04-29 20:36:11 +0800

    shell add sleeep3

--- centos_config/restartweb.sh
+++ centos_config/restartweb.sh
@@ -2,9 +2,11 @@
 
 service mysqld restart
 service nginx  restart
-service redis restart
+service redis stop
 killall -9 uwsgi
 uwsgi /home/blog/config.ini -d /home/log/uwsgi.log
 killall -9 celery
+sleep 3
+service redis start
 cd /home/blog
 celery worker -A celery_worker.celery -l INFO &
