ac526dc784730e3cc48d03d5eb0b76be4a876233
home_wen <ifwenvlook@163.com>
2016-05-12 13:18:07 +0800

    add admin_edituser

---
+++ app/admin/forms.py
@@ -0,0 +1,38 @@
+#encoding:utf-8
+from flask.ext.wtf import Form
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
+
+
+class AddadminForm(Form):
+    username = StringField('用户名', validators=[
+        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                          'Usernames must have only letters, '
+                                          'numbers, dots or underscores')])
+    password = PasswordField('密码', validators=[
+        Required(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('确认密码', validators=[Required()])
+    submit = SubmitField('添加')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已存在')
+
+
+class AdduserForm(Form):
+    email = StringField('邮箱', validators=[Required(), Length(1, 64),
+                                           Email()])
+    username = StringField('用户名', validators=[
+        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                          'Usernames must have only letters, '
+                                          'numbers, dots or underscores')])
+    password = PasswordField('密码', validators=[
+        Required(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('确认密码', validators=[Required()])
+    submit = SubmitField('添加')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已存在')
--- app/admin/views.py
+++ app/admin/views.py
@@ -6,14 +6,19 @@ from ..models import Permission, Role, User, Post, Comment, Message, Category, S
 from ..decorators import admin_required, permission_required
 from .. import db
 from . import admin
+from .forms import AddadminForm, AdduserForm
 
 
 @admin.route('/', methods=['GET', 'POST'])
 @login_required
 @admin_required
 def edit(): 
-	admins = User.query.filter_by(role_id=2).all()
-	return render_template('admin/admin.html',admins=admins)
+	page = request.args.get('page', 1, type=int)
+	pagination = User.query.filter_by(role_id=2).order_by(User.member_since.desc()).paginate(
+		page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
+		error_out=False)
+	admins = pagination.items
+	return render_template('admin/edit.html',admins=admins,pagination=pagination, page=page)
 
 
 @admin.route('/admin2user/<int:id>', methods=['GET', 'POST'])
@@ -25,3 +30,71 @@ def admin2user(id):
 	db.session.add(user)
 	flash ('已将" '+user.username+' "降为普通用户')
 	return redirect(url_for('.edit'))
+
+@admin.route('/addadmin', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def addadmin():
+	form = AddadminForm()	
+	if form.validate_on_submit():
+		user = User(email=form.username.data+'@vlblog.com',
+					username=form.username.data,
+					password=form.password.data,
+					confirmed=True,
+					role=Role.query.filter_by(permissions=0xff).first())
+		db.session.add(user)
+		db.session.commit()
+		flash('已添加" '+user.username+' "为管理员')
+		return redirect(url_for('.edit'))
+	return render_template('admin/addadmin.html',form=form)
+
+
+@admin.route('/edituser', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edituser():	
+	page = request.args.get('page', 1, type=int)
+	pagination = User.query.order_by(User.member_since.desc()).paginate(
+		page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
+		error_out=False)
+	users = pagination.items
+	return render_template('admin/edituser.html',users=users,pagination=pagination, page=page)
+
+@admin.route('/deleteuser/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def deleteuser(id):
+	user = User.query.get_or_404(id)
+
+	posts = user.posts
+	for post in posts:
+		db.session.delete(post)
+	comments = user.comments
+	for comment in comments:
+		db.session.delete(comment)
+	messages = user.messages
+	for message in messages:
+		db.session.delete(message)
+	webpushs = user.webpushs
+	for webpush in webpushs:
+		db.session.delete(webpush)
+
+	db.session.delete(user)
+	flash ('已将" '+user.username+' "及相关内容删除')
+	return redirect(url_for('.edituser'))
+
+@admin.route('/adduser', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def adduser():
+	form = AdduserForm()	
+	if form.validate_on_submit():
+		user = User(email=form.email.data,
+					username=form.username.data,
+					password=form.password.data,
+					confirmed=True)
+		db.session.add(user)
+		db.session.commit()
+		flash('已添加" '+user.username+' "为普通用户')
+		return redirect(url_for('.edituser'))
+	return render_template('admin/adduser.html',form=form)
