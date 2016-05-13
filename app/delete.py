36c8382add2298c422f8516f68421d2f6b2c505a
home_wen <ifwenvlook@163.com>
2016-05-13 21:03:22 +0800

    delete none

---
+++ app/delete.py
@@ -0,0 +1,10 @@
+from .models import  User
+from . import db
+
+
+
+def deletenone():
+	noneuser=User.query.filter_by(username=None).all()
+	for user in noneuser:
+		db.session.delete(user)
+	db.session.commit()
