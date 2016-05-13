1e4fc5227f3705ce4bc6987d7c48c9c542a0fef7
wen_centos <ifwenvlook@gmail.com>
2016-05-13 21:29:17 +0800

    mod nolog.sh

--- centos_config/nolog_restartweb.sh
+++ centos_config/nolog_restartweb.sh
@@ -2,8 +2,10 @@
 
 service mysqld restart
 service nginx  restart
-service redis restart
+service redis stop
+sleep 3
+service redis start
 killall -9 uwsgi
-uwsgi /home/blog/config.ini 
 cd /home/blog
-celery worker -A celery_worker.celery -l INFO
+celery worker -A celery_worker.celery -l INFO &
+uwsgi /home/blog/config.ini 
